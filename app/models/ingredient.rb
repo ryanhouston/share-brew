9b52f82faaad427683f599a706f927c60e9313d2
Ryan Houston <ryanhouston83@gmail.com>
2010-09-01 18:13:17 -0400

    Fix ingredient type validation
    Need to drop and recreate ingredients table or manually alter
    'type' column to 'ingredient_type'.  Think I was hitting a
    rails default attribute.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,6 @@ class Ingredient < ActiveRecord::Base
     has_many :recipes, :through => :recipe_ingredients
 
     validates_presence_of :name, :description
+    validates_inclusion_of :ingredient_type, :in => %w(malt hops yeast other),
+        :message => "%{value} is not a valid ingredient type"
 end
--- db/migrate/20100830182756_create_ingredients.rb
+++ db/migrate/20100830182756_create_ingredients.rb
@@ -3,7 +3,7 @@ class CreateIngredients < ActiveRecord::Migration
     create_table :ingredients do |t|
       t.string :name
       t.text :description
-      t.string :type
+      t.string :ingredient_type
 
       t.timestamps
     end

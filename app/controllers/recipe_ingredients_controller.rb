2fed5d76982a647011315582dad787fa300594f5
Ryan Houston <ryanhouston83@gmail.com>
2010-09-07 22:58:13 -0400

    CRUD working for RecipeIngredients

--- app/controllers/recipe_ingredients_controller.rb
+++ app/controllers/recipe_ingredients_controller.rb
@@ -3,6 +3,7 @@ class RecipeIngredientsController < ApplicationController
   # GET /recipe_ingredients.xml
   def index
     @recipe_ingredients = RecipeIngredient.all
+    @recipe = Recipe.find(params[:recipe_id])
 
     respond_to do |format|
       format.html # index.html.erb
@@ -35,17 +36,22 @@ class RecipeIngredientsController < ApplicationController
 
   # GET /recipe_ingredients/1/edit
   def edit
+    @recipe = Recipe.find(params[:recipe_id])
     @recipe_ingredient = RecipeIngredient.find(params[:id])
   end
 
   # POST /recipe_ingredients
   # POST /recipe_ingredients.xml
   def create
+    recipe = Recipe.find(params[:recipe_id])
+    ingredient = Ingredient.find(params[:recipe_ingredient][:ingredient])
+    params[:recipe_ingredient][:ingredient] = ingredient
+    params[:recipe_ingredient][:recipe] = recipe
     @recipe_ingredient = RecipeIngredient.new(params[:recipe_ingredient])
 
     respond_to do |format|
       if @recipe_ingredient.save
-        format.html { redirect_to(@recipe_ingredient, :notice => 'RecipeIngredient was successfully created.') }
+        format.html { redirect_to([recipe, @recipe_ingredient], :notice => 'RecipeIngredient was successfully created.') }
         format.xml  { render :xml => @recipe_ingredient, :status => :created, :location => @recipe_ingredient }
       else
         format.html { render :action => "new" }
@@ -58,10 +64,12 @@ class RecipeIngredientsController < ApplicationController
   # PUT /recipe_ingredients/1.xml
   def update
     @recipe_ingredient = RecipeIngredient.find(params[:id])
+    params[:recipe_ingredient][:recipe] = Recipe.find(params[:recipe_id])
+    params[:recipe_ingredient][:ingredient] = Ingredient.find(params[:recipe_ingredient][:ingredient])
 
     respond_to do |format|
       if @recipe_ingredient.update_attributes(params[:recipe_ingredient])
-        format.html { redirect_to(@recipe_ingredient, :notice => 'RecipeIngredient was successfully updated.') }
+        format.html { redirect_to([@recipe_ingredient.recipe, @recipe_ingredient], :notice => 'RecipeIngredient was successfully updated.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
@@ -74,10 +82,11 @@ class RecipeIngredientsController < ApplicationController
   # DELETE /recipe_ingredients/1.xml
   def destroy
     @recipe_ingredient = RecipeIngredient.find(params[:id])
+    recipe = @recipe_ingredient.recipe
     @recipe_ingredient.destroy
 
     respond_to do |format|
-      format.html { redirect_to(recipe_ingredients_url) }
+      format.html { redirect_to(recipe_recipe_ingredients_url(recipe)) }
       format.xml  { head :ok }
     end
   end

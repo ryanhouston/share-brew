498f529e907b5385320c99e4a811fc73496fc3d3
Ryan Houston <ryanhouston83@gmail.com>
2010-09-09 11:51:37 -0400

    Add convenience methods to recipe for ingredient types

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,4 +7,17 @@ class Recipe < ActiveRecord::Base
     validates_inclusion_of :mash_type, :in => ['extract', 'grain', 'partial']
 
     accepts_nested_attributes_for :recipe_ingredients
+
+    def malts
+      recipe_ingredients.joins(:ingredient).where('ingredients.ingredient_type = "malt"')
+    end
+
+    def hops
+      recipe_ingredients.joins(:ingredient).where('ingredients.ingredient_type = "hops"')
+    end
+
+    def yeast
+      recipe_ingredients.joins(:ingredient).where('ingredients.ingredient_type = "yeast"')
+    end
+
 end

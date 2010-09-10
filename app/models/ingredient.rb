ceac885dcc9d7a2a5f0dbbd4a0e4d0e7ed5713d8
Ryan Houston <ryanhouston83@gmail.com>
2010-09-10 18:56:47 -0400

    Add ingredient type scopes to ingredient

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,4 +4,9 @@ class Ingredient < ActiveRecord::Base
     validates_presence_of :name, :description
     validates_inclusion_of :ingredient_type, :in => %w(malt hops yeast other),
         :message => "%{value} is not a valid ingredient type"
+
+    scope :malt,  where(:ingredient_type => 'malt')
+    scope :hop,   where(:ingredient_type => 'hop')
+    scope :yeast, where(:ingredient_type => 'yeast')
+
 end

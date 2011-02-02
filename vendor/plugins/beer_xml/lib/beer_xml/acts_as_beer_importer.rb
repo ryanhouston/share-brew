195fc27949db02333197fcac73d38c5332617e4f
Ryan Houston <ryanhouston83@gmail.com>
2011-02-02 01:20:52 -0500

    Enable acts_as_beer_importer_of to accept callbacks
    Some attributes of a model using acts_as_beer_importer_of will need to
    do some transformations on the attributes provided from the datafile.
    Now a callback can be provided for each attribute requiring transforms

--- vendor/plugins/beer_xml/lib/beer_xml/acts_as_beer_importer.rb
+++ vendor/plugins/beer_xml/lib/beer_xml/acts_as_beer_importer.rb
@@ -4,22 +4,22 @@ module BeerXml
   end
 
   module ClassMethods
-    def acts_as_beer_importer type, options = nil
-      cattr_accessor :beer_importer_type, :attr_map
+    def acts_as_beer_importer_of type, options = nil
+      cattr_accessor :beer_importer_type, :attr_map, :translate_callbacks
       self.beer_importer_type = type
-      self.attr_map = options[:translated_as] if !options.nil?
+      self.attr_map = options[:translated_as] if options
+      self.translate_callbacks = options[:using] if options
       send :include, InstanceMethods
     end
 
     def import_from_hash style_hash
       beer_style = self.new
       beer_style.load_defaults style_hash
+      beer_style.run_translation_callbacks(style_hash) if self.translate_callbacks
       beer_style
     end
 
     def import_beer_xml filename
-      # TODO: Pass self.attr_map to Importer
-      # TODO: Importer needs ref to target class type
       @importer ||= BeerXml::Importer.new
       @importer.import filename
       @importer.send self.beer_importer_type, self
@@ -31,18 +31,34 @@ module BeerXml
     def load_defaults style_hash
       style_hash.each do |attr_key, attr_value|
         style_attr = attr_key.downcase
-        begin
-          send "#{style_attr}=", attr_value
-        rescue NoMethodError
-          # Check for a provided override
-          if !self.attr_map.nil? && !self.attr_map[style_attr.to_sym].nil?
-            mapped_attr = self.attr_map[style_attr.to_sym]
-            logger.info "Mapping beer XML <#{style_attr}> to [#{mapped_attr}]\n"
-            send "#{mapped_attr}=", attr_value
-          else
-            logger.info "No mapping for #{style_attr}\n"
-          end
-        end
+        attempt_set_attr style_attr, attr_value
+      end
+    end
+
+  private
+    def attempt_set_attr style_attr, attr_value
+      begin
+        send "#{style_attr}=", attr_value
+      rescue NoMethodError
+        load_override_if_provided(style_attr, attr_value)
+      end
+    end
+
+    def load_override_if_provided style_attr, attr_value
+      if !self.attr_map.nil? && !self.attr_map[style_attr.to_sym].nil?
+        mapped_attr = self.attr_map[style_attr.to_sym]
+        logger.debug "Mapping beer XML <#{style_attr}> to [#{mapped_attr}]\n"
+        send "#{mapped_attr}=", attr_value
+      else
+        logger.debug "No override mapping for #{style_attr}\n"
+      end
+    end
+
+  public
+    def run_translation_callbacks style_hash
+      self.translate_callbacks.each do |model_attr, model_method|
+        translated_value = send model_method, style_hash
+        send "#{model_attr}=", translated_value
       end
     end
 
--- vendor/plugins/beer_xml/test/acts_as_beer_importer_test.rb
+++ vendor/plugins/beer_xml/test/acts_as_beer_importer_test.rb
@@ -1,14 +1,19 @@
 require 'test_helper'
 
 class BeerStyle < ActiveRecord::Base
-  acts_as_beer_importer :styles
+  acts_as_beer_importer_of :styles
 end
 
 class UnusualBeerStyle < ActiveRecord::Base
   load_schema
 
-  acts_as_beer_importer :styles, :translated_as => {
-    :abv_max => :max_abv }
+  acts_as_beer_importer_of :styles,
+    :translated_as => { :abv_max => :max_abv },
+    :using => {:min_abv => :translate_min_abv}
+
+  def translate_min_abv beer_attrs
+    beer_attrs['ABV_MIN'].to_f * -1
+  end
 end
 
 class BeerXml::ActsAsBeerImporterTest < ActiveSupport::TestCase
@@ -26,9 +31,16 @@ class BeerXml::ActsAsBeerImporterTest < ActiveSupport::TestCase
       assert_equal 'American Ale', beer_styles.first.category
     end
 
-    should "be able to specify attribute mappings" do
-     beer_styles = UnusualBeerStyle.import_beer_xml load_file
-     assert_equal 6.0, beer_styles.first.max_abv
+
+    should "be able to map a beer XML property to another attribute" do
+      beer_styles = UnusualBeerStyle.import_beer_xml(load_file)
+      assert_equal 6.0, beer_styles.first.max_abv
+    end
+
+    should "be able to provide a call back for mapping beer XML attributes" do
+      beer_styles = UnusualBeerStyle.import_beer_xml(load_file)
+      assert_instance_of Array, beer_styles
+      assert_equal -4.5, beer_styles.first.min_abv
     end
   end
 

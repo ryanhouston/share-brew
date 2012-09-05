ff21c99ff08f5879ce2523a7c02b07d3bd3329b4
Ryan Houston <ryanhouston83@gmail.com>
2012-09-05 09:49:01 -0400

    Turn off asset pipeline for now so old styles load

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,7 @@ module ShareBrew
     config.filter_parameters += [:password]
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    config.assets.enabled = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'

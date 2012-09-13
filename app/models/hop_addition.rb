d0b2f9dc2fc111c77934752fce38403b993fdf56
Ryan Houston <ryanhouston83@gmail.com>
2012-09-13 17:23:51 -0400

    Nest hop additions in recipe form

--- app/models/hop_addition.rb
+++ app/models/hop_addition.rb
@@ -1,5 +1,5 @@
 class HopAddition < ActiveRecord::Base
   belongs_to :hop
   belongs_to :recipe
-  attr_accessible :hop, :alpha_acid, :beta_acid, :form, :duration, :weight, :use
+  attr_accessible :hop_id, :alpha_acid, :beta_acid, :form, :duration, :weight, :use
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,13 @@
 class Recipe < ActiveRecord::Base
+    attr_accessible :beer_style, :mash_type, :name, :batch_size, :description,
+      :original_gravity, :final_gravity, :bitterness, :color, :procedure,
+      :hop_additions_attributes
     belongs_to :beer_style
     belongs_to :user
     has_many   :hop_additions
+
+    accepts_nested_attributes_for :hop_additions, :allow_destroy => :true,
+      :reject_if => proc { |attrs| attrs['hop_id'].blank? }
 
     validates_presence_of :name, :batch_size, :description, :procedure
     validates_inclusion_of :mash_type, :in => ['extract', 'grain', 'partial']

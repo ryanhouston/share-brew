e62dc38dc035463b63714a3902075102dee355ac
Ryan Houston <ryanhouston83@gmail.com>
2012-09-19 16:44:42 -0400

    Add edit and destroy actions for yeast additions

--- app/controllers/yeast_additions_controller.rb
+++ app/controllers/yeast_additions_controller.rb
@@ -10,7 +10,25 @@ class YeastAdditionsController < ApplicationController
     @yeast_addition = @recipe.yeast_additions.build(params[:yeast_addition])
 
     if @yeast_addition.save
-      redirect_to(@recipe, :notice => "Yeast addition added")
+      redirect_to(edit_recipe_path(@recipe), :notice => "Yeast addition added")
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:recipe_id])
+    @yeast_addition = YeastAddition.find(params[:id])
+
+    render :new
+  end
+
+  def update
+    @yeast_addition = YeastAddition.find(params[:id])
+
+    if @yeast_addition.update_attributes(params[:yeast_addition])
+      flash[:notice] = "#{@yeast_addition.yeast.strain} yeast addition updated"
+      redirect_to(edit_recipe_path(@yeast_addition.recipe_id))
     else
       render :new
     end
@@ -19,7 +37,8 @@ class YeastAdditionsController < ApplicationController
   def destroy
     @yeast_addition = YeastAddition.find(params[:id])
     @yeast_addition.destroy
-    redirect_to(recipe_path(@yeast_addition.recipe_id))
+    flash[:notice] = "Removed yeast addition for " + @yeast_addition.yeast.strain
+    redirect_to(edit_recipe_path(@yeast_addition.recipe_id))
   end
 
 end

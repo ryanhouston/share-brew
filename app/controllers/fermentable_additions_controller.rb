9726df9b5de445ac2049e4d62038b7ab044ad963
Ryan Houston <ryanhouston83@gmail.com>
2012-09-21 09:49:06 -0400

    Forgot to git-add the fermentable additions

---
+++ app/controllers/fermentable_additions_controller.rb
@@ -0,0 +1,32 @@
+class FermentableAdditionsController < ApplicationController
+  def new
+    @recipe = Recipe.find(params[:recipe_id])
+    @fermentable_addition = @recipe.fermentable_additions.build
+  end
+
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @fermentable_addition = @recipe.fermentable_additions.build(params[:fermentable_addition])
+
+    if @fermentable_addition.save
+      flash[:notice] = "Added #{@fermentable_addition.fermentable.name} to recipe"
+      redirect_to edit_recipe_path(@recipe)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @fermentable_addition = FermentableAddition.find(params[:id])
+  end
+
+  def update
+    @fermentable_addition = FermentableAddition.find(params[:id])
+
+    if @fermentable_addition.update_attributes(params[:fermentable_addition])
+      redirect_to edit_recipe_path(@fermentable_addition.recipe)
+    else
+      render :edit
+    end
+  end
+end

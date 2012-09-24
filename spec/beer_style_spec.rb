ee0fecc2f6b62f6bef39cf6e3564b1d01b7659cd
Ryan Houston <ryanhouston83@gmail.com>
2012-09-24 00:10:44 -0400

    Use rspec errors_on helper

--- spec/beer_style_spec.rb
+++ spec/beer_style_spec.rb
@@ -10,43 +10,43 @@ describe BeerStyle do
   end
 
   it "should require name" do
-    style = FactoryGirl.build(:beer_style, :name => "").should_not be_valid
+    style = FactoryGirl.build(:beer_style, :name => "").should have(1).errors_on(:name)
   end
 
   it "should require unique name" do
-    style1 = FactoryGirl.create(:beer_style, :name => "not_unique").should be_valid
-    style2 = FactoryGirl.build(:beer_style, :name => "not_unique").should_not be_valid
+    FactoryGirl.create(:beer_style, :name => "not_unique").should be_valid
+    FactoryGirl.build(:beer_style, :name => "not_unique").should have(1).errors_on(:name)
   end
 
   it "should require a description" do
-    FactoryGirl.build(:beer_style, :description => "").should_not be_valid
+    FactoryGirl.build(:beer_style, :description => "").should have(1).errors_on(:description)
   end
 
   it "should require a category" do
-    FactoryGirl.build(:beer_style, :category => "").should_not be_valid
+    FactoryGirl.build(:beer_style, :category => "").should have(1).errors_on(:category)
   end
 
   it "should require min original gravity to be in proper range" do
-    FactoryGirl.build(:beer_style, :min_orig_grav => 0.0000).should_not be_valid
-    FactoryGirl.build(:beer_style, :min_orig_grav => 4.1).should_not be_valid
+    FactoryGirl.build(:beer_style, :min_orig_grav => 0.0000).should have(1).errors_on(:min_orig_grav)
+    FactoryGirl.build(:beer_style, :min_orig_grav => 4.1).should have(1).errors_on(:min_orig_grav)
     FactoryGirl.build(:beer_style, :min_orig_grav => 1.012).should be_valid
   end
 
   it "should require max original gravity to be in proper range" do
-    FactoryGirl.build(:beer_style, :max_orig_grav => 0.0000).should_not be_valid
-    FactoryGirl.build(:beer_style, :max_orig_grav => 4.1).should_not be_valid
+    FactoryGirl.build(:beer_style, :max_orig_grav => 0.0000).should have(1).errors_on(:max_orig_grav)
+    FactoryGirl.build(:beer_style, :max_orig_grav => 4.1).should have(1).errors_on(:max_orig_grav)
     FactoryGirl.build(:beer_style, :max_orig_grav => 1.012).should be_valid
   end
 
   it "should require min final gravity to be in proper range" do
-    FactoryGirl.build(:beer_style, :min_final_grav => 0.0000).should_not be_valid
-    FactoryGirl.build(:beer_style, :min_final_grav => 4.1).should_not be_valid
+    FactoryGirl.build(:beer_style, :min_final_grav => 0.0000).should have(1).errors_on(:min_final_grav)
+    FactoryGirl.build(:beer_style, :min_final_grav => 4.1).should have(1).errors_on(:min_final_grav)
     FactoryGirl.build(:beer_style, :min_final_grav => 1.012).should be_valid
   end
 
   it "should require max final gravity to be in proper range" do
-    FactoryGirl.build(:beer_style, :max_final_grav => 0.0000).should_not be_valid
-    FactoryGirl.build(:beer_style, :max_final_grav => 4.1).should_not be_valid
+    FactoryGirl.build(:beer_style, :max_final_grav => 0.0000).should have(1).errors_on(:max_final_grav)
+    FactoryGirl.build(:beer_style, :max_final_grav => 4.1).should have(1).errors_on(:max_final_grav)
     FactoryGirl.build(:beer_style, :max_final_grav => 1.012).should be_valid
   end
 

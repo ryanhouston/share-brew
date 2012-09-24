9cf3762744277289fa742dbbca084797ef08966d
Ryan Houston <ryanhouston83@gmail.com>
2012-09-24 00:00:55 -0400

    Move test factories to its own file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,9 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
+# Pull in defined factories
+require File.dirname(__FILE__) + "/factories"
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,30 +5,5 @@ require 'rails/test_help'
 require 'factory_girl'
 require 'redgreen'
 
-FactoryGirl.define do
-  factory :beer_style do
-    name 'Beer'
-    description 'A generic beer for testing.'
-    category 'American Ale'
-    min_orig_grav '1.038'
-    max_orig_grav '1.070'
-    min_final_grav '1.002'
-    max_final_grav '1.017'
-  end
-
-  factory :recipe do
-    name 'Recipe'
-    batch_size '1'
-    description 'A generic recipe for testing.'
-    procedure 'A generic procedure for testing.'
-    mash_type 'partial'
-  end
-
-  factory :yeast do
-    strain 'American Ale'
-    catalog_id '1056'
-    form 'liquid'
-    vendor 'Wyeast'
-  end
-end
+require File.dirname(__FILE__) + "/../spec/factories"
 

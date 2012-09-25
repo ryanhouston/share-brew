ac892db07f70a7d0d75512ea3d111c78b0df649c
Ryan Houston <ryanhouston83@gmail.com>
2012-09-24 23:20:54 -0400

    Convert hop tests to specs

---
+++ spec/models/hop_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Hop do
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :alpha_acid }
+  it { should validate_presence_of :form }
+  it { should validate_presence_of :typical_use }
+
+  context "The Hop class" do
+    it "acts as an importer of hops" do
+      Hop.should respond_to(:acts_as_beer_importer_of)
+      Hop.should respond_to(:import_beer_xml)
+    end
+  end
+end
+

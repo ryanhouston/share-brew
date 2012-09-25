ffa5cd4f99110f670bc436f68bd5f68161585d3e
Ryan Houston <ryanhouston83@gmail.com>
2012-09-24 22:55:09 -0400

    Convert last BeerStyle tests to specs

--- spec/models/beer_style_spec.rb
+++ spec/models/beer_style_spec.rb
@@ -15,5 +15,26 @@ describe BeerStyle do
   it { should ensure_inclusion_of(:min_final_grav).in_range(0.0001..4.0001) }
   it { should ensure_inclusion_of(:max_final_grav).in_range(0.0001..4.0001) }
   it { should have_many(:recipes) }
+
+  context "The BeerSTyle class" do
+    it "acts as a BeerXml importer" do
+      BeerStyle.should respond_to(:acts_as_beer_importer_of)
+      BeerStyle.should respond_to(:import_beer_xml)
+    end
+
+    it "can import a list of BeerXML style" do
+      styles = BeerStyle.import_beer_xml( load_styles_xml )
+      styles.should be_kind_of(Array)
+      styles.first.should be_kind_of(BeerStyle)
+      styles.first.min_abv.should eq 4.5
+      styles.first.max_abv.should eq 6.0
+      styles.first.category.should eq "American Ale - 10B"
+      styles.first.name.should eq "American Amber Ale"
+    end
+  end
+
+  def load_styles_xml
+    filename = Rails.root + 'test/fixtures/bjcp-2008-styles.xml'
+  end
 end
 

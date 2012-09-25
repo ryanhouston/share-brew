86d0f171552fa1b46dd0be60bdfe5e70e8fab25a
Ryan Houston <ryanhouston83@gmail.com>
2012-09-25 09:33:46 -0400

    Add some specs to recipe

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe Recipe do
   it { should belong_to(:beer_style) }
+  it { should belong_to(:user) }
+  it { should have_many(:fermentable_additions) }
+  it { should have_many(:hop_additions) }
+  it { should have_many(:yeast_additions) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:batch_size) }
   it { should validate_presence_of(:description) }

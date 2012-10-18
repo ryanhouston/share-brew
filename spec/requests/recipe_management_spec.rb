65a25aa0ec642bda30ee0212064167fe2bc9eab3
Ryan Houston <ryanhouston83@gmail.com>
2012-10-17 23:51:23 -0400

    Forgot to add spec/requests dir on last commit

---
+++ spec/requests/recipe_management_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'Recipe management' do
+
+  before (:each) do
+    @user = FactoryGirl.create(:user)
+    post new_user_session_path,
+      'user[email]' => @user.email, 'user[password]' => @user.password
+  end
+
+  it 'creates a new recipe before allowing ingredient additions' do
+    get '/recipes/new'
+    response.should render_template(:new)
+    response.body.should include "Vitals"
+  end
+end
+

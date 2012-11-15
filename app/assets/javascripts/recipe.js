7e8ac96fde8121cc792bc4af3a6d61bae59c9e68
Ryan Houston <ryanhouston83@gmail.com>
2012-11-15 11:46:26 -0500

    Set accept header for json instead of url appending
    Lets do this the right way with HTTP

--- app/assets/javascripts/recipe.js
+++ app/assets/javascripts/recipe.js
@@ -1,8 +1,12 @@
 $(function() {
   $(document).on('change', '#hop_addition_hop_id', function(event) {
-    $.get('/hops/' + $(this).val() + '.json', function(data) {
-      $('#hop_addition_alpha_acid').val(data.alpha_acid);
-      $('#hop_addition_beta_acid').val(data.beta_acid);
+    $.ajax({
+      url: '/hops/' + $(this).val(),
+      dataType: 'json',
+      success: function(data) {
+        $('#hop_addition_alpha_acid').val(data.alpha_acid);
+        $('#hop_addition_beta_acid').val(data.beta_acid);
+      }
     });
   });
 });

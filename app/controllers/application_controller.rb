1dd032f6339ed0dbe125b5003e5c66ad576ab808
Ryan Houston <ryanhouston83@gmail.com>
2012-11-19 21:36:30 -0500

    Add basic auth to production env
    I'm not really ready for this to be publically open. At the very least,
    new user accounts need to be verified by email which should help prevent
    spammy defacing of user input

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
 
   private
   def require_authentication
     unless user_signed_in?
       flash[:error] = "You must be logged in to perform this action"
       redirect_to new_user_session_path
+    end
+  end
+
+  def authenticate
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV["SHAREBREW_USER"] && password == ENV["SHAREBREW_PASSWD"]
+      end
     end
   end
 

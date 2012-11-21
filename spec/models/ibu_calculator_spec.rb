252347f3984bba3422fabfba3c3771385eb65b6d
Ryan Houston <ryanhouston83@gmail.com>
2012-11-21 13:00:03 -0500

    Modify ibu calculator spec module declaration

--- spec/models/ibu_calculator_spec.rb
+++ spec/models/ibu_calculator_spec.rb
@@ -1,23 +1,25 @@
 require 'spec_helper'
 require 'beer_science/ibu_calculator'
 
-describe BeerScience::IBUCalculator do
-  it "calculates IBUs given hop addition details" do
-    subject.IBUs_for_addition(
-      weight: 3.0,
-      alpha_acid: 4.4,
-      boil_time: 60,
-      boil_gravity: 1.060,
-      batch_size: 5.5
-    ).round(2).should == 37.95
-  end
+module BeerScience
+  describe IBUCalculator do
+    it "calculates IBUs given hop addition details" do
+      subject.IBUs_for_addition(
+        weight: 3.0,
+        alpha_acid: 4.4,
+        boil_time: 60,
+        boil_gravity: 1.060,
+        batch_size: 5.5
+      ).round(2).should == 37.95
+    end
 
-  it "calculates AAUs given a weight and alpha acid %" do
-    subject.AAUs_for_addition(2.0, 4.4).should == 8.8
-  end
+    it "calculates AAUs given a weight and alpha acid %" do
+      subject.AAUs_for_addition(2.0, 4.4).should == 8.8
+    end
 
-  it "provides the hop utilization rate for the boil" do
-    subject.hop_utilization(1.060, 60).round(3).should == 0.211
+    it "provides the hop utilization rate for the boil" do
+      subject.hop_utilization(1.060, 60).round(3).should == 0.211
+    end
   end
 end
 

42840023dae9b60caa4da2ef99de7d63899c4027
Ryan Houston <ryanhouston83@gmail.com>
2012-11-27 14:36:15 -0500

    Add missing Users field for devise password reset
    The field to track password resets was missing, likely because devise
    was initially setup in version 1.x and the field may not have existed at
    that point.
    
    Also fixes mailer configuration for development environment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121127145352) do
+ActiveRecord::Schema.define(:version => 20121127192714) do
 
   create_table "beer_styles", :force => true do |t|
     t.string   "name"
@@ -103,21 +103,22 @@ ActiveRecord::Schema.define(:version => 20121127145352) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                               :default => "",    :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "",    :null => false
-    t.string   "password_salt",                       :default => "",    :null => false
+    t.string   "email",                                 :default => "",    :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "",    :null => false
+    t.string   "password_salt",                         :default => "",    :null => false
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                       :default => 0
+    t.integer  "sign_in_count",                         :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                             :null => false
-    t.datetime "updated_at",                                             :null => false
-    t.boolean  "admin",                               :default => false
+    t.datetime "created_at",                                               :null => false
+    t.datetime "updated_at",                                               :null => false
+    t.boolean  "admin",                                 :default => false
     t.string   "name"
+    t.datetime "reset_password_sent_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true

ba9ddf701b35bf06eee9b881cc476a4403a76d90
Ryan Houston <ryanhouston83@gmail.com>
2012-12-05 21:46:26 -0500

    Fix up recipe listing with tabular formatting
    This shows the recipe overview better and will be able to add a jquery
    plugin to sort the columns and what not

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,11 +4,11 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
 
-    can :read, Recipe
-    can :create, Recipe unless user.new_record?
-    can [:update, :destroy], Recipe, user_id: user.id
-
     # Admins can do all the things
-    can :manage, :all if user.admin?
+    can(:manage, :all) if user.admin?
+
+    can :read, Recipe
+    can(:create, Recipe) unless user.new_record?
+    can [:update, :destroy], Recipe, user_id: user.id
   end
 end

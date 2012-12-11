61797b365f723a454ac4be3541d48c887a844804
Ryan Houston <ryanhouston83@gmail.com>
2012-12-11 17:13:33 -0500

    Fix up MashCalculator spec for docs to read better

--- spec/models/beer_science/mash_calculator_spec.rb
+++ spec/models/beer_science/mash_calculator_spec.rb
@@ -6,21 +6,21 @@ module BeerScience
     let (:batch_size) { 5.5 }
     let (:total_grain_weight) { 11 }
 
-    context "with no mash ratio specified" do
-      subject { MashCalculator.new(total_grain_weight, batch_size) }
+    subject { MashCalculator.new(total_grain_weight, batch_size) }
 
+    it "provides the volume of water required for the mash in quarts" do
+      subject.required_mash_water_volume_in_quarts.round(2).should == 17.37
+    end
+
+    it "provides the volume of water required for the mash in gallons" do
+      subject.required_mash_water_volume_in_gallons.round(2).should == 4.34
+    end
+
+    context "with no mash ratio specified" do
       it "provides a suggested mash ratio" do
         subject.suggested_mash_ratio.round(2).should == 1.58
       end
       its (:mash_ratio) { subject.round(2).should == 1.58 }
-
-      it "provides the volume of water required for the mash in quarts" do
-        subject.required_mash_water_volume_in_quarts.round(2).should == 17.37
-      end
-
-      it "provides the volume of water required for the mash in gallons" do
-        subject.required_mash_water_volume_in_gallons.round(2).should == 4.34
-      end
     end
 
     context "with a specified mash ratio" do

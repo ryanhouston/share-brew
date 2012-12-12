2f5f36af70d91308dd02645348517531754f787d
Ryan Houston <ryanhouston83@gmail.com>
2012-12-12 16:02:21 -0500

    Add preboil volume and boil off rate to mash calculator

--- app/models/beer_science/mash_calculator.rb
+++ app/models/beer_science/mash_calculator.rb
@@ -3,9 +3,10 @@ class MashCalculator
 
   attr_accessor :mash_ratio
 
-  def initialize( total_grain_weight, batch_size )
-    @total_grain_weight = total_grain_weight
-    @batch_size = batch_size
+  def initialize( total_grain_weight_lbs, batch_size_gallons, boil_minutes)
+    @total_grain_weight_lbs = total_grain_weight_lbs.to_f
+    @batch_size_gallons     = batch_size_gallons.to_f
+    @boil_minutes           = boil_minutes.to_f
   end
 
   def mash_ratio
@@ -15,12 +16,12 @@ class MashCalculator
   # Provides a suggested mash ratio based on the total amount of grain and the
   # preboil volume of wort required to reach the desired finishing batch size
   def suggested_mash_ratio
-    (preboil_volume + (preboil_volume*2 + 2*WATER_RETENTION_COEFFICIENT *
-      preboil_volume * @total_grain_weight)**0.5) / @total_grain_weight
+    (preboil_volume_gallons + (preboil_volume_gallons*2 + 2*WATER_RETENTION_COEFFICIENT *
+      preboil_volume_gallons * @total_grain_weight_lbs)**0.5) / @total_grain_weight_lbs
   end
 
   def required_mash_water_volume_in_quarts
-    suggested_mash_ratio * @total_grain_weight
+    suggested_mash_ratio * @total_grain_weight_lbs
   end
 
   def required_mash_water_volume_in_gallons
@@ -31,9 +32,12 @@ class MashCalculator
   # volume taking into account: amount of wort boiled off, loss to cooling
   # (with regard to water expansion due to temperature), loss to cooling
   # equipment (a counterflow chiller might trap some wort), and loss to trub.
-  def preboil_volume
-    7.5
+  def preboil_volume_gallons
+    boil_off_gallons + @batch_size_gallons
   end
 
+  def boil_off_gallons
+    (@boil_minutes.to_f / 60.to_f) * 1.to_f
+  end
 end
 
--- spec/models/beer_science/mash_calculator_spec.rb
+++ spec/models/beer_science/mash_calculator_spec.rb
@@ -5,27 +5,31 @@ module BeerScience
   describe MashCalculator do
     let (:batch_size) { 5.5 }
     let (:total_grain_weight) { 11 }
+    let (:boil_length) { 90 }
 
-    subject { MashCalculator.new(total_grain_weight, batch_size) }
+    subject { MashCalculator.new(total_grain_weight, batch_size, boil_length) }
 
     it "provides the volume of water required for the mash in quarts" do
-      subject.required_mash_water_volume_in_quarts.round(2).should == 17.37
+      subject.required_mash_water_volume_in_quarts.round(2).should == 16.54
     end
 
     it "provides the volume of water required for the mash in gallons" do
-      subject.required_mash_water_volume_in_gallons.round(2).should == 4.34
+      subject.required_mash_water_volume_in_gallons.round(2).should == 4.13
     end
 
+    its(:boil_off_gallons) { should == 1.50 }
+    its(:preboil_volume_gallons) { should == 7.00 }
+
     context "with no mash ratio specified" do
-      it "provides a suggested mash ratio" do
-        subject.suggested_mash_ratio.round(2).should == 1.58
+      its (:mash_ratio) { subject.round(4).should == 1.5036 }
+      it "provides the suggested mash ratio if none is provided" do
+        subject.mash_ratio.should == subject.suggested_mash_ratio
       end
-      its (:mash_ratio) { subject.round(2).should == 1.58 }
     end
 
     context "with a specified mash ratio" do
       subject do
-        calc = MashCalculator.new(total_grain_weight, batch_size)
+        calc = MashCalculator.new(total_grain_weight, batch_size, boil_length)
         calc.mash_ratio = 1.25
         calc
       end

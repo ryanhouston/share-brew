669f8aaaab95a18025e81e4ee216b4de18d794ab
Ryan Houston <ryanhouston83@gmail.com>
2014-10-01 00:40:38 -0400

    Updates for Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
   def require_authentication
     unless user_signed_in?
       flash[:error] = "You must be logged in to perform this action"
